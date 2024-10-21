896c3e6ea3fba1eb40d8f7c6031668f28149082b
David Omrai <david.omrai@seznam.cz>
2024-10-22 00:32:15 +0200

    #1 Updated the footer

--- sudety-raport-frontend/src/components/Footer/Footer.tsx
+++ sudety-raport-frontend/src/components/Footer/Footer.tsx
@@ -2,8 +2,8 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
-import { Facebook, Instagram, Twitter } from "@mui/icons-material";
-import { Box } from "@mui/material";
+import { Facebook, Instagram, Twitter, Send } from "@mui/icons-material";
+import { Box, TextField, InputAdornment, IconButton  } from "@mui/material";
 import './styles.css';
 
 export function Footer() {
@@ -22,26 +22,34 @@ export function Footer() {
         <Grid container spacing={5}>
           <Grid item xs={12} sm={4}>
             <Typography variant="h6" color="text.primary" gutterBottom>
-              About Us
+              Contact Us
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              We are XYZ company, dedicated to providing the best service to our
-              customers.
+                info@sudetyraport.com
             </Typography>
           </Grid>
           <Grid item xs={12} sm={4}>
             <Typography variant="h6" color="text.primary" gutterBottom>
-              Contact Us
+              Newsletter
             </Typography>
-            <Typography variant="body2" color="text.secondary">
-              123 Main Street, Anytown, USA
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Email: info@example.com
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Phone: +1 234 567 8901
-            </Typography>
+            <Grid container spacing={1}>
+              <Grid item sm={12}>
+              <TextField
+                  variant="outlined"
+                  label="Your Email"
+                  fullWidth
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton onClick={(e) => console.log(e)} edge="end">
+                          <Send />
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Grid>
+            </Grid>
           </Grid>
           <Grid item xs={12} sm={4}>
             <Typography variant="h6" color="text.primary" gutterBottom>
